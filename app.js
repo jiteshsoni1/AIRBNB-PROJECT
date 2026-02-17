f81fc4ad4bc73339e8c0836b31c71237a431d68a
jiteshsoni1 <jiteshsoni3591@gmail.com>
2026-02-17 22:39:10 +0530

    Fix MongoDB Atlas production connection

--- app.js
+++ app.js
@@ -22,8 +22,12 @@ const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 const { log } = require("console");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+let dbUrl = process.env.ATLASDB_URL;
 
+if (!dbUrl) {
+  dbUrl = "mongodb://127.0.0.1:27017/wanderlust";
+}
 // const dbUrl = process.env.ATLASDB_URL;
 
 main()
@@ -34,9 +38,13 @@ main()
     console.log(err);
   });
 
+// async function main() {
+//   await mongoose.connect(MONGO_URL);
+// }
 async function main() {
-  await mongoose.connect(MONGO_URL);
+  await mongoose.connect(dbUrl);
 }
+
 
 app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
